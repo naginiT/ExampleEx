141620fe9d672b5eb0afdfd7cfeaf7431d28aa4f
sada <jon7sada@gmail.com>
2018-09-19 16:56:18 +0530

    jenkinproject

---
+++ Jenkingit/src/demo/Demo.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void mthd()
+	{
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\NSSS KKISHORE\\Desktop\\chromedriver.exe");
+   WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.facebook.com/");
+	driver.manage().window().maximize();
+	}
+
+}
