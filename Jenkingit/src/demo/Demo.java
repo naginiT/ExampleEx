0c131576d98bd092895c566780d06c17151126bc
sadanandam <cubic@192.168.0.18>
2018-09-21 13:23:22 +0530

    committed

--- Jenkingit/src/demo/Demo.java
+++ Jenkingit/src/demo/Demo.java
@@ -15,8 +15,7 @@ public class Demo
 	driver.manage().window().maximize();
 	driver.get("https://www.facebook.com/");
 	driver.manage().window().maximize();
-    System.out.println("RojaPraveenr");
-    System.out.println("Rojareddy");
+ 
 	}
 
 }
