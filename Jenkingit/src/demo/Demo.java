319d8a857e654298a02c56259b9332f6788d4c45
sadanandam <cubic@192.168.0.18>
2018-09-21 14:36:10 +0530

    code

--- Jenkingit/src/demo/Demo.java
+++ Jenkingit/src/demo/Demo.java
@@ -15,7 +15,7 @@ public class Demo
 	driver.manage().window().maximize();
 	driver.get("https://www.facebook.com/");
 	driver.manage().window().maximize();
- 
+System.out.println("RojaPraveen");
 	}
 
 }
